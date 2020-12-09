901945bd754a0881ac739087f19da588bf06b46b
StartingCoding <StartingCodingPartner@gmail.com>
2020-12-09 11:46:39 +0100

    First build

---
+++ infOvid-portfolio/infOvid_portfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  infOvid_portfolioApp.swift
+//  infOvid-portfolio
+//
+//  Created by Loris on 12/9/20.
+//
+
+import SwiftUI
+
+@main
+struct infOvid_portfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
