b962cf7c74a144b5acf4d39545ed0ea262279d4e
StartingCoding <StartingCodingPartner@gmail.com>
2020-12-09 12:01:19 +0100

    Code for the main App struct

--- infOvid-portfolio/infOvid_portfolioApp.swift
+++ infOvid-portfolio/infOvid_portfolioApp.swift
@@ -9,9 +9,38 @@ import SwiftUI
 
 @main
 struct infOvid_portfolioApp: App {
+    @ObservedObject var data = DataDownloaded()
+    @ObservedObject var dataSettings = UserSettings()
+    @Environment(\.scenePhase) var scenePhase
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TabView {
+                ItalyView()
+                    .tabItem {
+                        Image(systemName: "globe")
+                        Text("italyTabBar")
+                    }
+                RegionList()
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                        Text("regionsTabBar")
+                    }
+            }
+            .environmentObject(data)
+            .environmentObject(dataSettings)
+        }
+        .onChange(of: scenePhase) { phase in
+            switch phase {
+            case .active:
+                data.fetchData()
+            case .inactive: break
+                // Do nothing
+            case .background: break
+                // Do nothing
+            @unknown default:
+                print("Some new different state implemented by Apple")
+            }
         }
     }
 }
